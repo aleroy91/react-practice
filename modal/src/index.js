9bd7ae8e0ea439a008a6e7e2e059d0a7871b728f
Alexander Le Roy <alexander.leroy@internetfusion.co.uk>
2022-07-13 09:13:29 +0100

    modal. Added some basic layout styling, refactored if statement to ternary

--- modal/src/index.js
+++ modal/src/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -16,11 +17,17 @@ function Price(props) {
 }
 
 function Avatar(props) {
-  return <img src={props.src} alt={props.name} onMouseEnter={() => props.onMouseEnter()} onMouseLeave={() => props.onMouseLeave()} />;
-}
-
-function MoreInfo(props) {
-  return <button>More Information</ button>
+  return (
+    <div className="image--container">
+      <img 
+        className="image" 
+        src={props.src} 
+        alt={props.name} 
+        onMouseEnter={() => props.onMouseEnter()} 
+        onMouseLeave={() => props.onMouseLeave()} 
+      />
+    </div>
+  );
 }
 
 function Player(props) {
@@ -28,23 +35,29 @@ function Player(props) {
   const gif = "https://media0.giphy.com/media/61nKC1dCr6yCDrAs3m/giphy.gif?cid=ecf05e47ol31uwoxeisjlc5f6xg4zypw7ilboas8mlhw0lon&rid=giphy.gif&ct=g";
   const [avatar, setAvatar] = useState(photo);
 
-  const toggleAvatar = () => setAvatar(() => {
-    if (avatar === photo) {
-      return gif;
-    } else {
-      return photo;
-    }
-  });
+  const toggleAvatar = () => setAvatar(() => (avatar === photo) ? gif: photo);
     
   return (
-    <div>
+    <div className="profile">
       <Name name="Bruno Fernandes" />
-      <Avatar src={avatar} alt={"Bruno Fernandes"} onMouseEnter={toggleAvatar} onMouseLeave={toggleAvatar} />
+      <Avatar 
+        src={avatar} 
+        alt={"Bruno Fernandes"} 
+        onMouseEnter={toggleAvatar} 
+        onMouseLeave={toggleAvatar} 
+      />
       <Position position="Midfield" />
       <Price price="10" />
-      <MoreInfo />
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal">
+      <Player/>
     </div>
   );
 }
     
-root.render(<Player />);
+root.render(<Modal />);
