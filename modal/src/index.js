18b633db180a2e74cf6a5f8cc33f2dd5313a2ae5
Alexander Le Roy <alexander.leroy@internetfusion.co.uk>
2022-07-19 10:12:01 +0100

    modal. Updated player data

--- modal/src/index.js
+++ modal/src/index.js
@@ -52,6 +52,13 @@ function Player(props) {
   );
 }
 
+function PlayerList(props) {
+  const players = props.players;
+  const playerList = players.map((player) => <li key={player.number}>{player.name}</li>);
+
+  return <ol>{playerList}</ol>;
+}
+
 function Modal(props) {
   return (
     <div className="modal">
@@ -59,5 +66,46 @@ function Modal(props) {
     </div>
   );
 }
+
+function Table(props) {
+  const players = [
+    {
+      number: 1,
+      name: 'Bruno Fernandes',
+      photo: 'https://resources.premierleague.com/premierleague/photos/players/110x140/p141746.png',
+      gif: 'https://media0.giphy.com/media/61nKC1dCr6yCDrAs3m/giphy.gif?cid=ecf05e47ol31uwoxeisjlc5f6xg4zypw7ilboas8mlhw0lon&rid=giphy.gif&ct=g',
+      position: "Midfield",
+      price: 10
+    }, {
+      number: 2,
+      name: 'Harry Maguire',
+      photo: 'https://resources.premierleague.com/premierleague/photos/players/110x140/p95658.png',
+      gif: 'https://media1.giphy.com/media/IhPL2nHX5cEdnvQ6mN/giphy.gif',
+      position: "Defence",
+      price: 5
+    }, {
+      number: 3,
+      name: 'Christian Eriksen',
+      photo: 'https://resources.premierleague.com/premierleague/photos/players/110x140/p80607.png',
+      gif: 'https://media.giphy.com/media/55iSqlzQ8CbYWnbCBn/giphy.gif',
+      position: "Midfield",
+      price: 6.5
+    }, {
+      number: 4,
+      name: 'Jadon Sancho',
+      photo: 'https://resources.premierleague.com/premierleague/photos/players/110x140/p209243.png',
+      gif: 'https://media.giphy.com/media/AbWzDpbWYTh9l1B3tc/giphy-downsized-large.gif',
+      position: "Midfield",
+      price: 7.5
+    } 
+  ];
+
+  return (<PlayerList players={players} />)
+}
     
-root.render(<Modal />);
+root.render(
+  <div className="container">
+    <Modal />
+    <Table />
+  </div>
+);
