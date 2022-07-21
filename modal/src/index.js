ccdb42215b2627785a537e5213b4da4ad724949d
Alexander Le Roy <alexander.leroy@internetfusion.co.uk>
2022-07-21 09:20:14 +0100

    modal. Changed player list into a table and applied some basic styling

--- modal/src/index.js
+++ modal/src/index.js
@@ -52,32 +52,36 @@ function Player(props) {
   );
 }
 
-function ListElement(props) {
+function TableRow(props) {
   return (
-    <li
+    <tr
       key={props.number}
       onClick={() => props.onClick(props.player)}
     >
-      {props.player.name}
-    </li>
+      <td className="table--cell">{props.player.name}</td>
+      <td className="table--cell">{props.player.position}</td>
+      <td className="table--cell">{props.player.price}</td>
+    </tr>
   ); 
 }
 
-function List(props) {
+function Table(props) {
   const players = props.players;
-  const playerList = players.map(
-    (player) => {
-      return (
-        <ListElement 
-          player={player}
-          number={player.number}
-          onClick={props.onClick} 
-        />
-      );
-    }
-  );
+  const playerTable = players.map((player) => 
+  <TableRow 
+    player={player}
+    number={player.number}
+    onClick={props.onClick} 
+  />);
 
-  return <ol>{playerList}</ol>;
+  return (
+    <table className="table">
+      <th className="table--header">Name</th>
+      <th className="table--header">Position</th>
+      <th className="table--header">Price</th>
+      {playerTable}
+    </table>
+  );
 }
 
 function Modal(props) {
@@ -88,7 +92,7 @@ function Modal(props) {
   );
 }
 
-function Table(props) {
+function Container(props) {
   const players = [
     {
       number: 1,
@@ -96,28 +100,32 @@ function Table(props) {
       photo: 'https://resources.premierleague.com/premierleague/photos/players/110x140/p141746.png',
       gif: 'https://media0.giphy.com/media/61nKC1dCr6yCDrAs3m/giphy.gif?cid=ecf05e47ol31uwoxeisjlc5f6xg4zypw7ilboas8mlhw0lon&rid=giphy.gif&ct=g',
       position: "Midfield",
-      price: 10
+      price: 10,
+      notes: ''
     }, {
       number: 2,
       name: 'Harry Maguire',
       photo: 'https://resources.premierleague.com/premierleague/photos/players/110x140/p95658.png',
       gif: 'https://media1.giphy.com/media/IhPL2nHX5cEdnvQ6mN/giphy.gif',
       position: "Defence",
-      price: 5
+      price: 5,
+      notes: ''
     }, {
       number: 3,
       name: 'Christian Eriksen',
       photo: 'https://resources.premierleague.com/premierleague/photos/players/110x140/p80607.png',
       gif: 'https://media.giphy.com/media/55iSqlzQ8CbYWnbCBn/giphy.gif',
       position: "Midfield",
-      price: 6.5
+      price: 6.5,
+      notes: ''
     }, {
       number: 4,
       name: 'Jadon Sancho',
       photo: 'https://resources.premierleague.com/premierleague/photos/players/110x140/p209243.png',
       gif: 'https://media.giphy.com/media/AbWzDpbWYTh9l1B3tc/giphy-downsized-large.gif',
       position: "Midfield",
-      price: 7.5
+      price: 7.5,
+      notes: ''
     } 
   ];
   let [selectedPlayer, setPlayer] = useState(players[0]);
@@ -126,9 +134,9 @@ function Table(props) {
   return (
     <div className="container">
       <Modal player={selectedPlayer} />
-      <List players={players} onClick={displayPlayer} />
+      <Table players={players} onClick={displayPlayer} />
     </div>
   );
 }
     
-root.render(<Table />);
+root.render(<Container />);
