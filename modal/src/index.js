53e3ddec139923a214a7c527ddafc2b453af11e2
Alexander Le Roy <alexander.leroy@internetfusion.co.uk>
2022-09-07 15:48:29 +0100

    interactive-table. Extensively refactored my work so far so that components are genericised and reusable, and names are more accurate descriptions

--- modal/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { mockData } from './data/mockData';
-import { TableWithMultipleRecordCards } from './components/table-with-multiple-record-cards';
-import './styles/main.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<TableWithMultipleRecordCards data={mockData} />);
