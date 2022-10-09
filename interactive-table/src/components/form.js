7aa7d897dc4cfde6db073ae26a499cee250f6ef3
Alexander Le Roy <alexander.leroy@internetfusion.co.uk>
2022-10-09 16:59:00 +0100

    interactive-table. Created form and input elements

---
+++ interactive-table/src/components/form.js
@@ -0,0 +1,21 @@
+import { Input } from './input';
+
+export const Form = props => {
+    const [name, formElements] = {...props};
+    const formElementsArray = Object.entries(formElements);
+
+    const formElementsFactory = () => {
+        for (const [formElementName, formElementType] of formElementsArray) {
+            return <Input key={index} inputName={formElementName} inputType={formElementType} />;
+        }
+    };
+
+    return (
+        <form>
+            <fieldset>
+                <legend>{name}</legend>
+                {formElementsFactory}
+            </fieldset>
+        </form>
+    );
+}
---
+++ interactive-table/src/components/input.js
@@ -0,0 +1,31 @@
+export const Input = props => {
+    const [inputName, inputType] = {...props};
+
+    const inputTypeFactory = inputType => {
+        switch(inputType) {
+            case "textarea":
+                return (
+                    <div>
+                        <textarea></textarea>
+                        <label>{inputName}</label>
+                    </div>
+                );
+            case "select":
+                return (
+                    <div>
+                        <select></select>
+                        <label>{inputName}</label>
+                    </div>
+                );
+            default:
+                return (
+                    <div>
+                        <input type={inputType} />
+                        <label>{inputName}</label>
+                    </div>
+                );
+        }
+    }
+
+    return {inputTypeFactory};
+}
