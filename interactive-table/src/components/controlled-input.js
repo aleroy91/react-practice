a5b5451d83b771dfee695a0da6132518432f6d33
Alexander Le Roy <alexander.leroy@internetfusion.co.uk>
2022-10-11 13:19:25 +0100

    interactive-table. Uncommented ControlledInput component as this works as expected

--- interactive-table/src/components/controlled-input.js
+++ interactive-table/src/components/controlled-input.js
@@ -1,37 +1,31 @@
 export const ControlledInput = (props) => {
   const { inputName, inputType } = { ...props };
 
-  // const inputTypeFactory = (inputType) => {
-  //   switch (inputType) {
-  //     case "textarea":
-  //       return (
-  //         <div>
-  //           <textarea></textarea>
-  //           <label>{inputName}</label>
-  //         </div>
-  //       );
-  //     case "select":
-  //       return (
-  //         <div>
-  //           <select></select>
-  //           <label>{inputName}</label>
-  //         </div>
-  //       );
-  //     default:
-  //       return (
-  //         <div>
-  //           <input type={inputType} />
-  //           <label>{inputName}</label>
-  //         </div>
-  //       );
-  //   }
-  // };
+  const inputTypeFactory = (inputType) => {
+    switch (inputType) {
+      case "textarea":
+        return (
+          <div>
+            <textarea></textarea>
+            <label>{inputName}</label>
+          </div>
+        );
+      case "select":
+        return (
+          <div>
+            <select></select>
+            <label>{inputName}</label>
+          </div>
+        );
+      default:
+        return (
+          <div>
+            <input type={inputType} />
+            <label>{inputName}</label>
+          </div>
+        );
+    }
+  };
 
-  // return <div>{inputTypeFactory(inputType)}</div>;
-  return (
-    <div>
-      <input type={inputType} />
-      <label>{inputName}</label>
-    </div>
-  );
+  return <div>{inputTypeFactory(inputType)}</div>;
 };
