685222aca838d21a6470074d90bf0e878984012b
Alexander Le Roy <alexjleroy@gmail.com>
2022-11-23 17:19:20 +0000

    interactive-table. Rearranged toolbar layout

--- interactive-table/src/components/toolbar.js
+++ interactive-table/src/components/toolbar.js
@@ -8,14 +8,19 @@ export const Toolbar = (props) => {
         <div className="toolbar--margin-bottom">
             <div className="toolbar">
                 <div className="horizontal-container">
-                    <h1 className="toolbar__header">Players Table</h1>
-                    <SearchBar filterData={filterData}/>
-                    <button className="button__info" onClick={() => displaySettings()}>
-                        <span className="material-icons">more_vert</span>
-                    </button>
+                    <div className="vertical-container">
+                        <div className="horizontal-container">
+                            <h1>Players Table</h1>
+                        </div>
+                        <div className="horizontal-container">
+                            <SearchBar filterData={filterData}/>
+                            <button className="button__info" onClick={() => displaySettings()}>
+                                <span className="material-icons">more_vert</span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
-
