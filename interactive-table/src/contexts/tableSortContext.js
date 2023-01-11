02f43db29c24146ed7ac01c9056495b8a73d46ac
Alexander Le Roy <alexjleroy@gmail.com>
2023-01-11 17:53:21 +0000

    interactive-table. Created TableSortContext to manage all sorting of table data

---
+++ interactive-table/src/contexts/tableSortContext.js
@@ -0,0 +1,82 @@
+import React, { useContext, useState } from "react";
+import { mockData } from "../data/mockData";
+import { useSelectedTableColumns } from "./tableColumnsContext";
+
+const TableSortOrderContext = React.createContext();
+const TableSortOrderUpdateContext = React.createContext();
+
+export const useTableSortOrder = () => {
+  return useContext(TableSortOrderContext);
+};
+
+export const useTableSortOrderUpdate = () => {
+  return useContext(TableSortOrderUpdateContext);
+};
+
+export const TableSortOrderProvider = ({ children }) => {
+  const selectedTableColumns = useSelectedTableColumns();
+
+  const [recordData, setRecordData] = useState(mockData);
+  const [sortOrder, setSortOrder] = useState(
+    selectedTableColumns.map(() => false)
+  );
+
+  const sortData = (columnName, columnIndex, highToLow) => {
+    const typeOfDataInColumnName = typeof recordData[0][columnName];
+
+    let sortedData = [...recordData];
+    let newSortOrder = sortOrder;
+
+    newSortOrder[columnIndex] = highToLow;
+    setSortOrder(newSortOrder);
+
+    if (typeOfDataInColumnName === "number") {
+      sortedData.sort((a, b) => {
+        if (highToLow) {
+          return b[columnName] - a[columnName];
+        } else {
+          return a[columnName] - b[columnName];
+        }
+      });
+    } else if (typeOfDataInColumnName === "string") {
+      sortedData.sort((a, b) => {
+        const columnNameA = a[columnName].toUpperCase();
+        const columnNameB = b[columnName].toUpperCase();
+
+        if (highToLow) {
+          if (columnNameA < columnNameB) {
+            return -1;
+          }
+
+          if (columnNameA > columnNameB) {
+            return 1;
+          }
+        } else {
+          if (columnNameA > columnNameB) {
+            return -1;
+          }
+
+          if (columnNameA < columnNameB) {
+            return 1;
+          }
+        }
+
+        return 0;
+      });
+    } else {
+      console.error(
+        "A sort was not performed as the data provided was neither of type string nor type number"
+      );
+    }
+
+    setRecordData(sortedData);
+  };
+
+  return (
+    <TableSortOrderContext.Provider value={sortOrder}>
+      <TableSortOrderUpdateContext.Provider value={setSortOrder}>
+        {children}
+      </TableSortOrderUpdateContext.Provider>
+    </TableSortOrderContext.Provider>
+  );
+};
--- interactive-table/src/index.js
+++ interactive-table/src/index.js
@@ -4,6 +4,7 @@ import { mockData } from "./data/mockData";
 import { defaultTableSettings } from "./data/defaultTableSettings";
 import { TableWithMultipleRecordCards } from "./components/table-with-multiple-record-cards";
 import { DisplaySelectedTableColumnsProvider } from "./contexts/tableColumnsContext";
+import { TableSortOrderProvider } from "./contexts/tableSortContext";
 import "./styles/main.css";
 import "./assets/fonts/Roboto-Black.ttf";
 import "./assets/fonts/Roboto-Regular.ttf";
@@ -12,9 +13,11 @@ import "./assets/fonts/Roboto-Light.ttf";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <DisplaySelectedTableColumnsProvider>
-    <TableWithMultipleRecordCards
-      data={mockData}
-      defaultTableSettings={defaultTableSettings}
-    />
+    <TableSortOrderProvider>
+      <TableWithMultipleRecordCards
+        data={mockData}
+        defaultTableSettings={defaultTableSettings}
+      />
+    </TableSortOrderProvider>
   </DisplaySelectedTableColumnsProvider>
 );
