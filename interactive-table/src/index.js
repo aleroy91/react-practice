686c3d88bb4f1176e880fa4a3421fd94a7ff70b4
Alexander Le Roy <alexjleroy@gmail.com>
2023-01-20 16:44:24 +0000

    interactive-table. Removed uneccessary context and references to it

--- interactive-table/src/index.js
+++ interactive-table/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { mockData } from "./data/mockData";
 import { defaultTableSettings } from "./data/defaultTableSettings";
 import { TableWithMultipleRecordCards } from "./components/table-with-multiple-record-cards";
-import { DisplaySelectedTableColumnsProvider } from "./contexts/tableColumnsContext";
 import { TableDataProvider } from "./contexts/tableDataContext";
 import "./styles/main.css";
 import "./assets/fonts/Roboto-Black.ttf";
@@ -13,11 +12,9 @@ import "./assets/fonts/Roboto-Light.ttf";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <TableDataProvider>
-    <DisplaySelectedTableColumnsProvider>
-      <TableWithMultipleRecordCards
-        data={mockData}
-        defaultTableSettings={defaultTableSettings}
-      />
-    </DisplaySelectedTableColumnsProvider>
+    <TableWithMultipleRecordCards
+      data={mockData}
+      defaultTableSettings={defaultTableSettings}
+    />
   </TableDataProvider>
 );
